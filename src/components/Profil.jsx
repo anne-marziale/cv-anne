fe14e96438de3e7b369ea4be6a88d0c5b65056b2
anne-marziale <annemarziale@hotmail.com>
2021-09-03 10:10:49 +0200

    cv corrected

--- src/components/Profil.jsx
+++ src/components/Profil.jsx
@@ -4,7 +4,7 @@ function Profil() {
     return (
         <div className="profil mb5">
             <h2 className="h2">Profil</h2>
-            <p>
+            <p className='text-presentation'>
             Je suis à la recherche d'une formation en alternance en tant que développeuse web et web mobile d'une durée d'un an. J'ai hâte de mettre en pratique les connaissances que j'ai acquises durant ma formation à la Wild Code School.
             </p>
         </div>
